eac1d61a446a30fffd324f9f18273af990d9bc8e
xachiperadzevasili <xachiperadzevasili@gmail.com>
2024-10-18 23:13:52 +0400

    commit

---
+++ Level 01/homework/hw.py
@@ -0,0 +1,8 @@
+print("name.  vasili")
+print("surname.  xachiperadze")
+print("age.  13")
+print("height.  1.66")
+
+
+print(" me mqvia vasili gvari xachiperadze. simagle 1.66. asaki 13. vswavlob goashi yvelaze magar academiashi sul es iyo naxvamdis.")
+      
