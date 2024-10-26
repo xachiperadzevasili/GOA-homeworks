68542f42fead98d8f18408f97b22ea9c103ea04a
xachiperadzevasili <xachiperadzevasili@gmail.com>
2024-10-26 09:10:21 +0400

    commit

---
+++ level 05/class/cw.py
@@ -0,0 +1,18 @@
+print(900 + 800)
+print(700 + 400)
+print(1000 + 756)
+
+
+print(5000 - 776)
+print(8000000 - 5000000)
+print(23333 - 11111)
+
+
+print(30 * 4)
+print(44 * 9)
+print(78 * 6)
+
+
+print(100 / 5)
+print(300 / 5)
+print(700 / 5)
