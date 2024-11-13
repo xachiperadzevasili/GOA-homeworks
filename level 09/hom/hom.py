7c4c870bcb095e3162ca91e0c0283d58f26e2636
xachiperadzevasili <xachiperadzevasili@gmail.com>
2024-11-13 22:34:45 +0400

    commit

---
+++ level 09/hom/hom.py
@@ -0,0 +1,18 @@
+print(10 > 5)
+print(10 > 5)
+print(10 > 5)
+print(10 > 5)
+print(10 > 5)
+
+print(90 < 180)
+print(90 < 180)
+print(90 < 180)
+print(90 < 180)
+print(90 < 180)
+
+
+print(40 = 40 )
+print(40 = 40 )
+print(40 = 40 )
+print(40 = 40 )
+print(40 = 40 )
