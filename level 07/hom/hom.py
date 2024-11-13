010c813ad493ffe5b0a5352338bd8b0f8d263a46
xachiperadzevasili <xachiperadzevasili@gmail.com>
2024-11-13 21:14:06 +0400

    commit

---
+++ level 07/hom/hom.py
@@ -0,0 +1,54 @@
+imput(ჩემი სახელი და გვარი არის ვასილი ხაჭიპერაძე. მე ვარ 13 წლის. ვცხოვრობ საქარტველოში კერძოდ თბილისში. შემი საყვარელი მანქანა არის BMW. ჩემი საყვარელი ფერი არის შვი შვი და შავი. )
+
+
+
+print(5 + 9)
+print(5 + 9)
+print(5 + 9)
+print(5 + 9)
+print(5 + 9)
+print(5 + 9)
+print(5 + 9)
+print(5 + 9)
+print(5 + 9)
+print(5 + 9)
+
+
+
+print(9 - 3)
+print(9 - 3)
+print(9 - 3)
+print(9 - 3)
+print(9 - 3)
+print(9 - 3)
+print(9 - 3)
+print(9 - 3)
+print(9 - 3)
+print(9 - 3)
+
+
+
+print(4 * 8)
+print(4 * 8)
+print(4 * 8)
+print(4 * 8)
+print(4 * 8)
+print(4 * 8)
+print(4 * 8)
+print(4 * 8)
+print(4 * 8)
+print(4 * 8)
+
+
+
+
+print(200 / 5)
+print(200 / 5)
+print(200 / 5)
+print(200 / 5)
+print(200 / 5)
+print(200 / 5)
+print(200 / 5)
+print(200 / 5)
+print(200 / 5)
+print(200 / 5)
